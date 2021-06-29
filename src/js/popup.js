61243111c671d28da5006646b4c0e3a595c73863
Karina Maulitova <karinamaulitova@gmail.com>
2021-06-29 18:14:18 +0300

    fix: submit preventDefault

--- src/js/popup.js
+++ src/js/popup.js
@@ -5,32 +5,32 @@ const popupForm = document.querySelector('.popup__form');
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
-  document.addEventListener("keydown", (evt) => handleEscClose(evt, popup));
-  
+  document.addEventListener('keydown', (evt) => handleEscClose(evt, popup));
 }
 
 function closePopup(popup) {
-    popup.classList.remove('popup_opened');
-    document.removeEventListener("keydown", (evt) => handleEscClose(evt, popup));
+  popup.classList.remove('popup_opened');
+  document.removeEventListener('keydown', (evt) => handleEscClose(evt, popup));
 }
 
 function handleEscClose(evt, popup) {
-  if (evt.key === "Escape") {
+  if (evt.key === 'Escape') {
     closePopup(popup);
   }
-};
-
+}
 
 popupOpenButton.addEventListener('click', () => openPopup(popup));
 popupCloseButton.addEventListener('click', () => closePopup(popup));
-popupForm.addEventListener('submit', () => closePopup(popup));
+popupForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  closePopup(popup);
+});
 
-popup.addEventListener("click", (evt) => {
-  if (evt.target.classList.contains("popup_opened")) {
+popup.addEventListener('click', (evt) => {
+  if (evt.target.classList.contains('popup_opened')) {
     closePopup(popup);
   }
-  if (evt.target.classList.contains("popup__close-button")) {
+  if (evt.target.classList.contains('popup__close-button')) {
     closePopup(popup);
   }
 });
-
